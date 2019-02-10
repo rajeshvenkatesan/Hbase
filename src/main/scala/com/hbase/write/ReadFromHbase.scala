a203d705d8909c891f5e131692757d43743d8e85
rajesh <rajeshvenkatesan1995@gmail.com>
2019-02-10 14:16:45 +0530

    Read from Hbase using Full scan Table

---
+++ src/main/scala/com/hbase/write/ReadFromHbase.scala
@@ -0,0 +1,73 @@
+package com.hbase.write
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client.Connection
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.sql.SparkSession
+
+import com.hbase.utils.Constants
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructType
+import java.util.List
+
+object ReadFromHbase extends Constants {
+  def getHbaseConfig(): Configuration = {
+    val conf = HBaseConfiguration.create();
+    conf.set("hbase.zookeeper.quorum", HBASE_ZOOKEEPER_QUORUM);
+    conf.set("hbase.zookeeper.property.clientPort", HBASE_CLIENT_PORT)
+    conf
+  }
+  def convertArrayOfBytesToString(bytes: Array[Byte]) = {
+    bytes.filter(p => p != null).map(_.toChar).mkString
+  }
+  def fullScanTable(spark: SparkSession, conf: Configuration, tableName: String,
+                    rowKeys: Seq[String], columnsToRead: Seq[String], dataFrameAliasName: Seq[String]):org.apache.spark.sql.DataFrame= {
+
+    import spark.implicits._
+
+    val connection: Connection = ConnectionFactory.createConnection(conf)
+    val admin = new HBaseAdmin(conf)
+    var columnsReadForAllRowKeys = Array[Row]()
+    if (admin.isTableAvailable(tableName)) {
+
+      val table = connection.getTable(TableName.valueOf(tableName))
+
+      for (rowKey <- rowKeys) {
+        var columnsReadForRowKey = Array[String]()
+        val hbaseRow = table.get(new Get(Bytes.toBytes(rowKey)))
+
+        columnsToRead.foreach(f => {
+          val columnFamily = f.split(":")(0)
+          val columnQualifierName = f.split(":")(1)
+          val arrayOfBytes: Array[Byte] = hbaseRow.getValue(Bytes.toBytes(columnFamily), Bytes.toBytes(columnQualifierName))
+
+          val column = convertArrayOfBytesToString(arrayOfBytes)
+          columnsReadForRowKey = columnsReadForRowKey :+ column
+        })
+        Row(columnsReadForRowKey:_*)
+        columnsReadForAllRowKeys =columnsReadForAllRowKeys:+Row(columnsReadForRowKey:_*)
+      }
+
+    }
+    val schema = StructType(dataFrameAliasName.map(fieldName ⇒ StructField(fieldName, StringType, true)))
+    val resultDataFrame=spark.sqlContext.createDataFrame(spark.sparkContext.parallelize(columnsReadForAllRowKeys), schema)
+    resultDataFrame
+  }
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder().appName("Demo").master("local").getOrCreate()
+    import spark.implicits._
+    val conf = getHbaseConfig
+    val rowKeys = Seq("7111689861")
+    val columnsToRead = Seq("i:value", "i:productImage")
+    val dataFrameAliasName = Seq("valuesss", "ProductImage")
+    val result=  fullScanTable(spark, conf, "emp1", rowKeys, columnsToRead, dataFrameAliasName)
+    result.show
+   
+  }
+}
